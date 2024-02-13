61e051613d60f3d83e80a63788c4bcedf5cdc1d6
Sara Whittingslow <swhittings17@gmail.com>
2024-02-12 22:01:39 -0500

    added queries.sql

---
+++ queries.sql
@@ -0,0 +1,10 @@
+ SELECT * FROM owners  LEFT JOIN vehicles ON owners.id = vehicles.owner_id;
+
+  SELECT first_name,last_name,COUNT(owner_id) FROM owners JOIN vehicles ON owners.id = vehicles.owner_id
+  GROUP BY (first_name,last_name) ORDER BY first_name;
+
+  SELECT first_name,last_name,ROUND(AVG(price)) AS avg_price,COUNT(owner_id) FROM owners JOIN vehicles ON owners.id = vehicles.owner_id  
+  GROUP BY (first_name,last_name) HAVING COUNT(owner_id) > 1 AND ROUND(AVG(price)) > 10000 ORDER BY first_name desc;
+
+
+
